cd39171df4486627da2d694d8ac5cc823c443ae8
Sean Derrow <sderrow@mit.edu>
2018-01-20 04:36:52 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,5 +1,17 @@
 //define functions here
 
+function getIt() {
+  $("p").on("click", function() {
+    alert("Hey!")
+  })
+}
+
+function frameIt() {
+  $("img").on("load", function() {
+    this.className = "tasty"
+  })
+}
+
 $(document).ready(function(){
 
 // call functions here
