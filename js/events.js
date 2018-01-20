0c1617107883b3d81d48f737fa0ac8541819e8d1
Sean Derrow <sderrow@mit.edu>
2018-01-20 04:45:05 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,35 @@
 //define functions here
 
+function getIt() {
+  $("p").on("click", function() {
+    alert("Hey!")
+  })
+}
+
+function frameIt() {
+  $("img").on("load", function() {
+    this.className = "tasty"
+  })
+}
+
+function pressIt() {
+  $("input").on("keydown", function(e) {
+    const key = parseInt(e.detail || e.which);
+    if (key === 71) {
+      alert("You pressed the 'G' key!")
+    }
+  })
+}
+
+function submitIt() {
+  $("form").on("submit", function() {
+    alert("Your form is going to be submitted now.")
+  })
+}
+
 $(document).ready(function(){
-
-// call functions here
-
+  getIt()
+  frameIt()
+  pressIt()
+  submitIt()
 });
